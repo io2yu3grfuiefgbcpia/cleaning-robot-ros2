3d3f62c8907009a1d318dd5cf5df340598aba459
io2yu2gfuiefgbcpia <yys@example.com>
2026-06-12 10:35:39 +0800

    修复导航包启动文件安装

--- src/cleaning_robot_navigation/setup.py
+++ src/cleaning_robot_navigation/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'cleaning_robot_navigation'
@@ -10,6 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
